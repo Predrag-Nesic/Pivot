08d315d1df7316646842d7f243b4e3c54a2098f2
Predrag Nesic <54184774+Predrag-Nesic@users.noreply.github.com>
2023-06-20 17:12:31 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] niz = {13, 19, 9, 5, 12, 8, 7, 14, 21, 2, 6, 11};
+        int[] niz = {15, 22, 7, 13, 11, 10, 72, 25, 33, 1, 8};
         Razvrstavanje r = new Razvrstavanje(niz);
         r.razvrstaj(niz, 0, niz.length - 1);
 
